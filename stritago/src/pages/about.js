22566e0ca530cbab232acc3d4b5ede4b3b2a6dcd
Raphael Gebauer <gebauerraphael@web.de>
2021-03-09 17:24:48 +0100

    new

--- stritago/src/pages/about.js
+++ stritago/src/pages/about.js
@@ -1,12 +1,13 @@
 import React from "react"
+import Layout from "../components/layout"
 import Header from "../components/header"
 
 export default function About() {
   return (
-    <div style={{ color: `teal` }}>
+    <Layout>
       <Header headerText="About Gatsby y" />
       <Header headerText="It's pretty cool" />
       <p>Such wow. Very React.</p>
-    </div>
+    </Layout>
   )
 }
--- stritago/src/pages/contact.js
+++ stritago/src/pages/contact.js
@@ -1,13 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/layout"
 import Header from "../components/header"
 
 export default function Contact() {
   return (
-    <div style={{ color: `teal` }}>
-      <Link to="/">Home</Link>
+    <Layout>
       <Header headerText="Contact" />
       <p>Send us a message!</p>
-    </div>
+      <p>
+        <a href="mailto:me@example.com">me@example.com</a>
+      </p>
+    </Layout>
   )
 }
